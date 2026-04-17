96147aaa3691ecca3bf19ed9d2d4e857a2eb1163
mynameisjec <140921351+mynameisjec@users.noreply.github.com>
2026-04-17 19:45:03 -0400

    Fix index.js for hosting

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 require('dotenv').config();
-require('./src/index.js');
+
+const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+  partials: [Partials.Message, Partials.Reaction, Partials.User],
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'src', 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if (command.data && command.execute) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+const eventsPath = path.join(__dirname, 'src', 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.BOT_TOKEN);
