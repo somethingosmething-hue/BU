52e2af5cdc1e7abf30d9e354a185333ad35352da
mynameisjec <140921351+mynameisjec@users.noreply.github.com>
2026-04-18 14:26:30 -0400

    Add index.js entry point

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-// First time setup
+const repoUrl = 'https://github.com/somethingosmething-hue/BU.git';
+
 if (!fs.existsSync('./src')) {
   console.log('📦 Cloning repo...');
-  execSync('git clone --depth 1 https://github.com/somethingosmething-hue/BU .', { stdio: 'inherit' });
+  execSync('git clone --depth 1 ' + repoUrl + ' .', { stdio: 'inherit' });
   console.log('📦 Installing dependencies...');
   execSync('npm install', { stdio: 'inherit' });
 }
 
-// Ensure .env exists
 if (!fs.existsSync('./.env')) {
   fs.writeFileSync('./.env', 'BOT_TOKEN=YOUR_BOT_TOKEN_HERE\n');
 }
 
-// Load dotenv and start bot
 require('dotenv').config();
 const client = require('./src/index');
