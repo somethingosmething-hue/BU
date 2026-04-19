54798e6d67b832e7476b8b3a3325ffea9950585d
somethingosmething-hue <somethingosmething@gmail.com>
2026-04-18 23:06:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
-
+ 
 const repoUrl = 'https://github.com/somethingosmething-hue/BU.git';
-
+ 
 if (!fs.existsSync('./src')) {
-  console.log('📦 Cloning repo...');
-  execSync('git clone --depth 1 ' + repoUrl + ' .', { stdio: 'inherit' });
-  console.log('📦 Installing dependencies...';
+  console.log('Cloning repo...');
+  execSync('git clone --depth 1 ' + repoUrl + ' _tmp_clone', { stdio: 'inherit' });
+  execSync('cp -r _tmp_clone/. .', { stdio: 'inherit' });
+  execSync('rm -rf _tmp_clone', { stdio: 'inherit' });
+  console.log('Installing dependencies...');
   execSync('npm install', { stdio: 'inherit' });
 }
-
-if (!fs.existsSync('./.env')) {
-  fs.writeFileSync('./.env', 'BOT_TOKEN=YOUR_BOT_TOKEN_HERE\n');
-}
-
+ 
 require('dotenv').config();
 const client = require('./src/index');
