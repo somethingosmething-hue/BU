dcd2229d972e04d70d567e528b06e9a8f1961fe9
somethingosmething-hue <somethingosmething@gmail.com>
2026-04-18 23:18:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
- 
+
 const repoUrl = 'https://github.com/somethingosmething-hue/BU.git';
- 
+
 if (!fs.existsSync('./src')) {
   console.log('Cloning repo...');
   execSync('git clone --depth 1 ' + repoUrl + ' _tmp_clone', { stdio: 'inherit' });
@@ -10,7 +10,16 @@ if (!fs.existsSync('./src')) {
   execSync('rm -rf _tmp_clone', { stdio: 'inherit' });
   console.log('Installing dependencies...');
   execSync('npm install', { stdio: 'inherit' });
+} else if (fs.existsSync('./.git')) {
+  console.log('Pulling latest changes...');
+  try {
+    execSync('git fetch origin main', { stdio: 'inherit' });
+    execSync('git reset --hard origin/main', { stdio: 'inherit' });
+    console.log('Updated successfully.');
+  } catch (e) {
+    console.log('Git update failed, continuing with existing files.');
+  }
 }
- 
+
 require('dotenv').config();
 const client = require('./src/index');
