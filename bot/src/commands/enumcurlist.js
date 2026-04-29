56d71526b5cd77e2382650dc4405e95674233357
mynameisjec <140921351+mynameisjec@users.noreply.github.com>
2026-04-29 18:10:51 -0400

    Allow /eshowcurlist anywhere, add /enumcurlist to count global list elements

--- bot/src/commands/enumcurlist.js
+++ bot/src/commands/enumcurlist.js
@@ -3,16 +3,11 @@ const db = require('../database/db');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('eshowcurlist')
-    .setDescription('Show and remove first N elements from a global curlist (ephemeral, DM only)')
+    .setName('enumcurlist')
+    .setDescription('Show how many elements are in a global curlist')
     .addStringOption(opt =>
       opt.setName('name')
         .setDescription('Name of the global curlist')
-        .setRequired(true)
-    )
-    .addIntegerOption(opt =>
-      opt.setName('number')
-        .setDescription('Number of elements to show and remove')
         .setRequired(true)
     ),
   async execute(interaction) {
@@ -24,22 +19,12 @@ module.exports = {
     }
 
     const name = interaction.options.getString('name');
-    const number = interaction.options.getInteger('number');
-
-    if (number < 1) {
-      return interaction.reply({ content: 'Number must be at least 1.', ephemeral: true });
-    }
-
     const curlist = await db.getGlobalCurList(name);
 
     if (!curlist || curlist.elements.length === 0) {
       return interaction.reply({ content: `No elements found in global list "${name}".`, ephemeral: true });
     }
 
-    const toShow = curlist.elements.slice(0, number);
-    const remaining = curlist.elements.slice(number);
-
-    await interaction.reply({ content: `[GLOBAL LIST: ${name}]\n${toShow.join('\n')}`, ephemeral: true });
-    await db.saveGlobalCurList(name, remaining);
+    await interaction.reply({ content: `🌐 **${name}** has **${curlist.elements.length}** element(s).`, ephemeral: true });
   },
 };
